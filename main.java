3d6c15739ca0f02b456a4e9997a5e0484d8c5589
joshankumar6299 <joshankumar6299@gmail.com>
2025-11-06 21:59:40 +0530

    add the logical operator and to the code

---
+++ main.java
@@ -0,0 +1,48 @@
+public class main {
+    public static void main(String[] args) {
+        // System.out.println("Hello world!");
+
+        // int num1= 4;
+        // int num2=5;
+        // int result=num1 + num2;
+        // System.out.println(result);
+
+        // byte b = 127;
+        // int a = 12;
+        // byte k=(byte)a;
+
+        // float f = 5.6f;
+        // int t = (int)f;
+        // System.out.println(t);
+
+        // byte a = 10;
+        // byte b = 30;
+        // int result = a* b;
+        // System.out.println(result);
+
+        // operaters
+
+        int num1 = 5;
+        int num2 =8;
+        int result0 = num1 + num2;
+        int result1 = num1 - num2;
+        int result2 = num1 * num2;
+        int result3 = num1 / num2;
+        int result4 = num1 % num2;
+        System.out.println(result0);
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+        // ++num1;  // post - increment
+        // num1++;  // pre - increment
+        // --num1;  // post - decrement
+        // num1--;  // pre - decrement
+        
+        
+
+
+
+    }
+
+}

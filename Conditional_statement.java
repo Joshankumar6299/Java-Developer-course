10a23947d02f4408ea95393494f71a6d3b0c2153
joshankumar6299 <joshankumar6299@gmail.com>
2025-11-06 23:09:15 +0530

    add this

---
+++ Conditional_statement.java
@@ -0,0 +1,48 @@
+
+
+public class Conditional_statement {
+    public static void main(String[] args) {
+
+        // int x = 8;
+        // int y = 7;
+        // int z = 9;
+
+        // if(x>y && x>z)  // false
+        //     System.out.println(x);
+        // else if(y>x && y>z)   // false
+        //     System.out.println(y);
+        // else   // true
+        //     System.out.println(z);
+        
+
+
+        // if(x>10){
+        //     System.out.println("x is greater than 10");
+        // }
+        // else if(x<10){
+        //     System.out.println("x is less than 10");
+        // }
+        // else{
+        //     System.out.println("x is equal to 10");
+        
+        // }
+
+
+        int n = 8;
+        int result = 0;
+
+        //  if(n%2==0)
+        //     System.err.println("even");
+        // else
+        //     System.out.println("odd");
+        
+
+        // ternary operator ?:
+        
+        result = n%2==0 ? 10 : 20;
+        System.out.println(result);
+        
+    
+
+    }
+}
---
+++ loops.java
@@ -0,0 +1,44 @@
+public class loops {
+    public static void main(String[] args) {
+        
+        // While loop
+
+        // int i = 1;
+        // while (i<=5) { 
+        //     System.out.println("Hi"+i);
+        //     i++;
+        // }
+
+
+        // int i= 1;
+        // while(i<=4){
+        //     System.out.println("Hi " + i);
+        //     int j = 1;
+        //     while(j<=3){
+        //         System.out.println("Hello " + j);
+        //         j++;
+        //     }
+        //     i++;
+
+        // }
+
+
+        // Do while loop
+
+    //     int i= 5;
+    //    do{
+    //         System.out.println("Hi " + i);
+    //         i++;
+    //     } while(i<=4);
+
+
+    // For loops
+
+       
+       for(int i=1; i<=4; i++){
+            System.out.println("Hi " + i);
+            
+        } 
+        
+    }
+}

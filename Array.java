438c8a874f405e3bf51057645bf9dccede632603
joshankumar6299 <joshankumar6299@gmail.com>
2025-11-08 15:59:40 +0530

    add code

---
+++ Array.java
@@ -0,0 +1,44 @@
+public class Array {
+    public static void main(String[] args) {
+
+        // int nums[] = new int [4]; 
+        // nums[0] = 10; 
+        // nums[1] = 20; 
+        // nums[2] = 30; 
+        // nums[3] = 40; 
+        // for(int i=0;i<=3;i++){ 
+        // System.out.println(nums[i]); 
+        // } 
+        // System.out.println(nums[3]);
+
+
+        // int nums[][] = new int[3][4];
+        int nums[][] = new int[3][];  // jagged array
+        nums[0] = new int[4];
+        nums[1] = new int[3];
+        nums[2] = new int[2];
+
+        // fill with random numbers
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                nums[i][j] = (int)(Math.random() * 10);
+            }
+        }
+
+        // print using normal for loop
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[i].length; j++) {
+                System.out.print(nums[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        // print using enhanced for-each loop
+        for (int[] row : nums) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+}
